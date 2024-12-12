437fc7ca352dc0fc3f568ccd7b7235390bf8bc27
loicmaes <97891718+loicmaes@users.noreply.github.com>
2024-12-12 16:03:00 +0100

    :rocket: Add default auth page
    Signed-off-by: loicmaes <97891718+loicmaes@users.noreply.github.com>

---
+++ pages/auth.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import PageRoot from "~/components/composing/page/PageRoot.vue";
+
+const { t } = useI18n();
+</script>
+
+<template>
+  <PageRoot name="auth">
+    <div>{{ t("auth") }}</div>
+  </PageRoot>
+</template>
