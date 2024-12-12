0e4e546a753e34d578eaa0661bce25086d8a26d7
loicmaes <97891718+loicmaes@users.noreply.github.com>
2024-12-12 16:02:52 +0100

    :rocket: Setup welcome component properly
    Signed-off-by: loicmaes <97891718+loicmaes@users.noreply.github.com>

--- assets/i18n/en.ts
+++ assets/i18n/en.ts
@@ -1,3 +1,10 @@
 export default {
-  helloWorld: "Hello World!",
+  temp: {
+    welcome: {
+      title: "Welcome to Nuxt!",
+      caption: "Start your project with functional authentication and operational front/back route protection! A revolution for project kick-offs to avoid redundant work in designing a private application.",
+      visitDoc: "Get started",
+      authProcess: "Start testing",
+    },
+  },
 };
