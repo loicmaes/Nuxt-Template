af2d36ae35a708a9e15052ab3a3e18d52cb54619
loicmaes <97891718+loicmaes@users.noreply.github.com>
2024-12-18 10:31:29 +0100

    :rocket: Setup ui configuration components (language and theme)
    Signed-off-by: loicmaes <97891718+loicmaes@users.noreply.github.com>

---
+++ components/frontSettings/ThemeSelector.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import { Laptop, SunLight, HalfMoon } from "@iconoir/vue";
+
+const { t } = useI18n();
+const colorMode = useColorMode();
+</script>
+
+<template>
+  <DropdownMenu>
+    <DropdownMenuTrigger>
+      <Button
+        size="icon"
+        variant="outline"
+      >
+        <Laptop v-if="colorMode.preference === 'system'" />
+        <SunLight v-if="colorMode.preference === 'light'" />
+        <HalfMoon v-if="colorMode.preference === 'dark'" />
+      </Button>
+    </DropdownMenuTrigger>
+    <DropdownMenuContent
+      align="end"
+      side="top"
+    >
+      <DropdownMenuRadioGroup v-model="colorMode.preference">
+        <DropdownMenuRadioItem value="light">
+          <SunLight />
+          <span>{{ t("globals.themes.light") }}</span>
+        </DropdownMenuRadioItem>
+        <DropdownMenuRadioItem value="dark">
+          <HalfMoon />
+          <span>{{ t("globals.themes.dark") }}</span>
+        </DropdownMenuRadioItem>
+        <DropdownMenuRadioItem value="system">
+          <Laptop />
+          <span>{{ t("globals.themes.system") }}</span>
+        </DropdownMenuRadioItem>
+      </DropdownMenuRadioGroup>
+    </DropdownMenuContent>
+  </DropdownMenu>
+</template>
